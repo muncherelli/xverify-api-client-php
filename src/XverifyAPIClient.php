157a88fa8d61335f656c59aac71f86e3251095bf
muncherelli <16467105+muncherelli@users.noreply.github.com>
2023-10-11 10:52:00 -0500

    Initial commit

--- src/XverifyAPIClient.php
+++ src/XverifyAPIClient.php
@@ -26,43 +26,17 @@ class XverifyAPIClient {
         $this->client = new Client(array_merge($defaultConfig, $configOptions));
     }
 
-    public function verifyEmail(string $email = '', array $options = []): array {
-        if (empty($email)) {
-            return [
-                'status' => 'error',
-                'message' => $this->getStatusMessage(400),
-                'status_code' => 400
-            ];
+      public function verifyEmail(string $email = '', array $options = []): array {
+            $response = $this->makeRequest(endpoint: 'ev', params: ['email' => $email] + $options);
+            return $this->formatResponse(response: $response);
         }
-        
-        $response = $this->makeRequest(endpoint: 'ev', params: ['email' => $email] + $options);
-        return $this->formatResponse(response: $response);
-    }
 
     public function verifyPhone(string $phone = '', array $options = []): array {
-        if (empty($phone)) {
-            return [
-                'status' => 'error',
-                'message' => $this->getStatusMessage(400),
-                'status_code' => 400
-            ];
-        }
-        
         $response = $this->makeRequest(endpoint: 'pv', params: ['phone' => $phone] + $options);
         return $this->formatResponse(response: $response);
     }
 
     public function verifyAddress(array $params = []): array {
-        // required parameters
-        if (empty($params['address1']) || (empty($params['city']) && empty($params['zip']))) {
-            return [
-                'status' => 'error',
-                'message' => $this->getStatusMessage(400),
-                'status_code' => 400
-            ];
-        }
-        
-        // add the API key and domain to the parameters
         $params['api_key'] = $this->apiKey;
         $params['domain'] = $this->domain;
 
@@ -71,50 +45,64 @@ class XverifyAPIClient {
     }
 
     public function verifyCombined(array $params = []): array {
-        if (empty($params['email']) && empty($params['phone']) && empty($params['address1'])) {
-            return [
-                'status' => 'error',
-                'message' => $this->getStatusMessage(400),
-                'status_code' => 400
-            ];
-        }
-
         $response = $this->makeRequest(endpoint: 'aio', params: $params);
         return $this->formatResponse(response: $response);
     }
 
-
     private function makeRequest(string $endpoint, array $params): array {
-        // merge the API key and domain into the request params
         $params = array_merge($params, [
             'api_key' => $this->apiKey,
             'domain' => $this->domain
         ]);
-        // try to make the request, and handle any exceptions
+
         try {
-            $response = $this->client->get(uri: $endpoint, options: ['query' => $params]);
+            $response = $this->client->get(uri: $endpoint, options: ['query' => $params, 'http_errors' => false]); // Add 'http_errors' => false
+
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode >= 400) { 
+                return [
+                    'error' => $response->getReasonPhrase(),
+                    'statusCode' => $statusCode
+                ];
+            }
+
             return ['data' => json_decode($response->getBody(), true)];
         } catch (ConnectException $e) {
             return $this->handleConnectException(exception: $e);
-        } catch (RequestException $e) {
-            return $this->handleRequestException(exception: $e);
         } catch (\Exception $e) {
-            // catch any other exceptions that may occur and return them as an error
             return ['error' => $e->getMessage(), 'statusCode' => 0];
         }
     }
+
 
     private function handleConnectException(ConnectException $exception): array {
         return ['error' => 'Connection timeout or network issue.', 'statusCode' => 0];
     }
 
-    private function handleRequestException(RequestException $exception): array {
-        // if the exception has a response, we can get the status code and error message from it
-        // otherwise, just return a generic error message
-        $statusCode = $exception->getResponse() ? $exception->getResponse()->getStatusCode() : 0;
-        $error = $exception->hasResponse() ? (string) $exception->getResponse()->getBody() : 'Unknown error';
-        return ['error' => $error, 'statusCode' => $statusCode];
+private function handleRequestException(RequestException $exception): array {
+    if ($exception->hasResponse()) {
+        $response = $exception->getResponse();
+        $statusCode = $response->getStatusCode();
+        $reason = $response->getReasonPhrase();
+        
+        return [
+            'error' => $reason,
+            'statusCode' => $statusCode
+        ];
     }
+    
+    // If no response is available, return a generic error
+    return ['error' => 'Unknown error', 'statusCode' => 0];
+}
+
+
+
+
+
+
+
+
 
     // recursively filter out any empty fields from the response (or any nested arrays)
     private function filterEmptyFields(array $data): array {
@@ -129,28 +117,19 @@ class XverifyAPIClient {
         return $data;
     }
 
-    // format the response to match the expected format
-    private function formatResponse(array $response): array {
-        $data = $response['data'] ?? []; // the api's response data
-        $statusCode = $response['statusCode'] ?? 200; // the http status code
-
-        // if the response data is empty, return a generic error message
-        if (empty($data)) {
-            $result = array_merge($data, [
-                'message' => $this->getStatusMessage(statusCode: $statusCode),
-                'status_code' => $statusCode,
-                'status' => 'error'
-            ]);
-        } else {
-            // Return the API's response combined with the custom fields
-            $result = array_merge($data, [
-                'message' => $this->getStatusMessage(statusCode: $statusCode),
-                'status_code' => $statusCode
-            ]);
-        }
-        // filter out any empty fields from the response
-        return $this->filterEmptyFields($result);
+private function formatResponse(array $response): array {
+    if (isset($response['data'])) {
+        $decodedData = $response['data'];
+        return is_array($decodedData) ? $decodedData : [];
+    } elseif (isset($response['error'])) {
+        return [
+            'error' => $response['error'],
+            'statusCode' => $response['statusCode']
+        ];
     }
+    return ['error' => 'Unknown error', 'statusCode' => 0];
+}
+
 
     // return a status message based on the http status code
     private function getStatusMessage(int $statusCode): string {
